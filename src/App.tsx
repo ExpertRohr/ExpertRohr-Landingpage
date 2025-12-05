0fbe60c88d758e951316214cd09ad013069e5393
Marius Labitzke <info@expertrohr.de>
2025-12-05 04:33:51 +0100

    Update mobile call button number

--- src/App.tsx
+++ src/App.tsx
@@ -1832,7 +1832,7 @@ export default function App() {
                 Notdienst jetzt anrufen
               </span>
               <span className="text-sm sm:text-base font-bold leading-tight">
-                030&nbsp;–&nbsp;12345678
+                030&nbsp;–&nbsp 23323873;
               </span>
             </span>
 
